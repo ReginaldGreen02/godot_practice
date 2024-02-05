45a82630fbfb74a5aa618743d6ea529e7f74f4c8
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-05 12:36:47 -0600

    Inputs

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,7 +3,7 @@ extends Node2D
 var test_array: Array[String] = ["Test", "Hello", "stuff"]
 
 func _ready():
-	$Logo.rotation_degrees= 90
+	$Logo.rotation_degrees = 180
 	
 	#for i in test_array:
 		#print(i)
