ed9d168659d047665a4606fa7670317b3a5d87db
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-12 12:04:30 -0600

    RigidBody2D

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,12 +7,15 @@ func _on_gate_player_entered_gate(_body):
 	print("player has entered")
 	
 
-func _on_player_laser_fired(pos):
-	var laser = laser_scene.instantiate()
+func _on_player_laser_fired(pos, direction):
+	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
 	$Projectiles.add_child(laser)
 	
-func _on_player_grenade_thrown(pos):
-	var grenade =  grenade_scene.instantiate()
+func _on_player_grenade_thrown(pos,direction):
+	var grenade =  grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser_fired()
-signal grenade_thrown()
+signal laser_fired(pos, direction)
+signal grenade_thrown(pos,direction)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -12,15 +12,18 @@ func _process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
 	velocity = direction * 500
 	move_and_slide()
+	#rotate player
+	look_at(get_global_mouse_position())
 	
 	#laser shooting input
+	var player_direction  = (get_global_mouse_position() - position).normalized()
 	if Input.is_action_pressed("primary action") and can_laser:
 		#Randomly select marker 2D for laser
 		var laser_markers = $LaserStartPosition.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$laser_cd.start()
-		laser_fired.emit(selected_laser.global_position)
+		laser_fired.emit(selected_laser.global_position,player_direction)
 		
 		
 		
@@ -29,7 +32,7 @@ func _process(_delta):
 		var pos = $LaserStartPosition.get_children()[1].global_position
 		can_grenade = false
 		$grenade_cd.start()
-		grenade_thrown.emit(pos)
+		grenade_thrown.emit(pos,player_direction)
 	
 func test_function():
 	print("This is a test function")
