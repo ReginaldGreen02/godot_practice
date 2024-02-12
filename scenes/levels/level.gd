4d4a3706073d74bde67ec24e4b186110fc4f3888
ReginaldGreen02 <rejogreen@gmail.com>
2024-02-12 10:43:56 -0600

    Signals

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+
+func _on_gate_player_entered_gate(body):
+	print("player has entered")
+	print(body)
+
+func _on_player_laser_fired():
+	print("laser from level")
+
+func _on_player_grenade_thrown():
+	print("grenade from level")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+signal laser_fired()
+signal grenade_thrown()
+
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta):
 	
 	#input
@@ -8,11 +14,27 @@ func _process(_delta):
 	move_and_slide()
 	
 	#laser shooting input
-	if Input.is_action_pressed("primary action"):
-		print("laser fired")
+	if Input.is_action_pressed("primary action") and can_laser:
+		laser_fired.emit()
+		can_laser = false
+		$laser_cd.start()
+		
+		
+		
 	
-	if Input.is_action_pressed("secondary action"):
-		print("grenade thrown")
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		grenade_thrown.emit()
+		can_grenade = false
+		$grenade_cd.start()
 	
 func test_function():
 	print("This is a test function")
+
+
+func _on_laser_cd_timeout():
+	can_laser = true
+	
+
+
+func _on_grenade_cd_timeout():
+	can_grenade = true
